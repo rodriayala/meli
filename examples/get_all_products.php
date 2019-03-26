e24018fd1721eed3f6b6bae7426057ee68456080
rodriayala <ayalarodrigo50@gmail.com>
2019-03-26 11:27:08 -0300

    primeras mejoras, probando traer todos los productos

---
+++ examples/get_all_products.php
@@ -0,0 +1,33 @@
+<?php
+require '../Meli/meli.php';
+require '../configApp.php';
+
+$meli = new Meli($appId, $secretKey);
+
+$params = array();
+
+#$url = '/sites/' . $siteId;
+
+$url = "/users/{Cust_id}/items/search?access_token=$ACCESS_TOKEN";
+
+$result = $meli->get($url, $params);
+
+
+
+/*$params = array(
+'q' => 'dvd'
+);
+ 
+$result = $meli->get('/sites/MLV/search', $params);
+*/
+
+
+/*
+$listings = $meli -> get('/users/' . $user_id . '/items/search', array('status'=>'active', 'seller' => $user_id, 'access_token' => $access_token));
+echo 'listings: --------------------------<br>';
+print_r($listings['body']->results);
+*/
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';

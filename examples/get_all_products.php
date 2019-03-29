8d1d516ffe919e7770cedd489a777989950ce366
rodriayala <ayalarodrigo50@gmail.com>
2019-03-29 11:03:13 -0300

    pruebas de logueo 5

--- examples/get_all_products.php
+++ examples/get_all_products.php
@@ -21,8 +21,15 @@ $params = array();
 $result = $meli->get('/sites/MLV/search', $params);
 */
 
-echo "access_token:   ".$access_token = $_SESSION['access_token'];
+//echo "access_token:   ".$access_token = $_SESSION['access_token'];
 echo "user_id:    ".$user_id	  = $_SESSION['client_id'];
+
+
+//Traer el token de usuario
+#https://developers.mercadolibre.com.ar/es_ar/server-side
+#https://api.mercadolibre.com/oauth/token?grant_type=client_credentials&client_id=YOUR_CLIENT_ID&client_secret=YOUR_SECRET_KEY
+
+
 
 $listings = $meli -> get('/users/' . $user_id . '/items/search', array('status'=>'active', 'seller' => $user_id, 'access_token' => $access_token));
 echo 'LISTANDINGGGG: --------------------------<br>';

85a9a58b1882fa6d97d526459ffaa7ab9022c20f
Rodrigo Ayala <rodrigo@fmed.uba.ar>
2019-04-01 18:52:38 -0300

    pruebas de productos

--- actions/get_all_products.php
+++ actions/get_all_products.php
@@ -27,8 +27,11 @@ echo '</pre>';
 
 $result = $listings['body']->results;
 
-foreach ($result as $arr){
-	
+$X=0;
+foreach ($result as $arr)
+{
+	echo "Numero $X".$arr[$X]."<br>";
+	$X++;
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">

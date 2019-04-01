355b6f747099acb81d655f3dbf4edbeb6216c176
rodriayala <ayalarodrigo50@gmail.com>
2019-03-31 22:50:11 -0300

    pruebas sobre el PUBLICACION DE ITEMS

--- actions/publish_item.php
+++ actions/publish_item.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require 'Meli/meli.php';
-require 'configApp.php';
+require '../Meli/meli.php';
+require '../configApp.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">

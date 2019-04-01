a38b4cdbabb675ecc9736353dddc8d6ad7828d2c
rodriayala <ayalarodrigo50@gmail.com>
2019-03-31 22:48:56 -0300

    pruebas sobre el PUBLICACION DE ITEMS

--- actions/publish_item.php
+++ actions/publish_item.php
@@ -2,11 +2,6 @@
 session_start();
 require 'Meli/meli.php';
 require 'configApp.php';
-
-$domain = $_SERVER['HTTP_HOST'];
-$appName = explode('.', $domain)[0];
-
-$muestro_acciones = false;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">

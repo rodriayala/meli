59a93f853164562dcc9c32bd955269da3079a5f4
Rodrigo Ayala <rodrigo@fmed.uba.ar>
2019-04-12 17:15:44 -0300

    pruebas sobre un producto

--- actions/get_one_produc.php
+++ actions/get_one_produc.php
@@ -68,6 +68,7 @@ foreach($array_description as $arr_des)
 	<script src="../js/bootstrap-3.3.7.js"></script>
     
     <link rel="stylesheet" href="../css/box.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 </head>
 <body>
 <?php include('../menu.php'); ?>
